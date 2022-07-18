5ac61bb841d4620b71d0b5451484c9df7b128d2c
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2022-07-18 08:28:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Coding Temple Week 3 Day 5 Homework
+
+###### CT-week3-day5-homework
+
+**Note: It doesn't do anything at the moment**
---
+++ __main__.py
@@ -0,0 +1,23 @@
+from gui import open_window 
+
+if __name__ == "__main__":
+	open_window()
+
+'''
+place bet
+dealer gives one card to all players and one to themselves
+everyone gets another face up card
+dealer gets another face down card
+
+if you get 21 right off the bat, you get 2.5x your bet
+
+hit / stay
+
+after everyone stays or busts, the deal flips up their facedown card
+if their hand is >= 17: stay
+else: hit
+
+if hand is > deal and <= 21: win 2x your bet 
+else: lose, lol
+
+'''
